61f279174c90cf091d932fe460ecb46e1ecf54a0
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2021-03-04 18:00:14 +0100

    Remove hack to clean up TMX

--- scripts/update_tmx.sh
+++ scripts/update_tmx.sh
@@ -7,7 +7,3 @@ cd $root_path/data
 
 echo "Downloading updated TMX from Pontoon"
 curl -o it.all-projects.tmx --compressed https://pontoon.mozilla.org/translation-memory/it.all-projects.tmx
-
-# Temporary fix for 1681653
-echo "Fixing chr(3) error"
-sed -i '' $'s/\x03//g' it.all-projects.tmx
