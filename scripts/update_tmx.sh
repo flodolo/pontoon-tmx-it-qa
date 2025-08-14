fa43e7612c71924d6fe309cbbfcbee958acee5a4
Francesco Lodolo <flod@lodolo.net>
2025-08-14 08:07:28 +0200

    Improve TMX download script (retry on fail)

--- scripts/update_tmx.sh
+++ scripts/update_tmx.sh
@@ -6,4 +6,12 @@ root_path=$script_path/..
 cd $root_path/data
 
 echo "Downloading updated TMX from Pontoon"
-curl -o it.all-projects.tmx --compressed https://pontoon.mozilla.org/translation-memory/it.all-projects.tmx
+# Retry up to 5 times, wait 10 seconds between retries
+curl \
+  --fail \
+  --compressed \
+  --retry 5 \
+  --retry-delay 10 \
+  --retry-connrefused \
+  -o it.all-projects.tmx \
+  https://pontoon.mozilla.org/translation-memory/it.all-projects.tmx
