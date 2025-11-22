5280d50547d62008c3b2a150087aa7ba49bbc1d3
Francesco Lodolo <flod@lodolo.net>
2025-11-22 07:33:00 +0100

    Check for minimum size when downloading TMX

--- scripts/update_tmx.sh
+++ scripts/update_tmx.sh
@@ -12,6 +12,7 @@ output="it.all-projects.tmx"
 
 max_attempts=5
 delay=10
+min_size=$((1024 * 1024)) # 1 MB
 
 for ((attempt=1; attempt<=max_attempts; attempt++)); do
     echo "Attempt $attempt of $max_attempts..."
@@ -25,12 +26,15 @@ for ((attempt=1; attempt<=max_attempts; attempt++)); do
       -o "$output" \
       "$url"
 
-    # Check if download succeeded and file is non-empty
-    if [ -s "$output" ]; then
-        echo "✅ Download succeeded and file is not empty."
+    # GitHub seems to download a 4 Kb file when failing, so need to check for
+    # a minimum size.
+    filesize=$(wc -c <"$output")
+    if [ "$filesize" -ge "$min_size" ]; then
+        size_human=$(du -h "$output" | cut -f1)
+        echo "✅ Download succeeded, file size OK ($size_human)."
         break
     else
-        echo "⚠️ File is empty or download failed. Retrying in $delay seconds..."
+        echo "⚠️ File too small ($filesize bytes). Retrying in $delay seconds..."
         sleep "$delay"
     fi
 done
